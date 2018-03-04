437000ab5075576916190b5130360057676dfc55
shijiacheng <iot_shijiacheng@163.com>
2018-03-04 17:02:32 +0800

    归并排序

--- src/com/shijc/coding/SortUtils.java
+++ src/com/shijc/coding/SortUtils.java
@@ -7,15 +7,16 @@ public class SortUtils {
 
     /**
      * 冒泡排序
+     *
      * @param array
      */
-    public void bubbleSort(int[] array){
-        for (int i = array.length-1; i >0 ; i--) {
+    public void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j] > array[j+1]){
+                if (array[j] > array[j + 1]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
@@ -23,19 +24,20 @@ public class SortUtils {
 
     /**
      * 选择排序
+     *
      * @param array
      */
-    public void selectionSort(int[] array){
-        for (int i = 0; i < array.length-1; i++) {
+    public void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             int k = i;
-            for (int j = k+1; j < array.length; j++) {
+            for (int j = k + 1; j < array.length; j++) {
 
-                if (array[j]<array[k]){
+                if (array[j] < array[k]) {
                     k = j;
                 }
             }
 
-            if (i!=k){
+            if (i != k) {
                 int temp = array[i];
                 array[i] = array[k];
                 array[k] = temp;
@@ -46,12 +48,13 @@ public class SortUtils {
 
     /**
      * 快速排序
+     *
      * @param array
      * @param low
      * @param high
      */
-    public void quickSort(int[] array,int low,int high){
-        if (low>high){
+    public void quickSort(int[] array, int low, int high) {
+        if (low > high) {
             return;
         }
 
@@ -59,15 +62,15 @@ public class SortUtils {
         int j = high;
         int key = array[low];
 
-        while (i<j){
-            while (key<=array[j]&&i<j){
+        while (i < j) {
+            while (key <= array[j] && i < j) {
                 j--;
             }
-            while (key>=array[i]&&i<j){
+            while (key >= array[i] && i < j) {
                 i++;
             }
 
-            if (i<j){
+            if (i < j) {
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
@@ -76,10 +79,55 @@ public class SortUtils {
 
         array[low] = array[i];
         array[i] = key;
-        quickSort(array,low,j-1);
-        quickSort(array,j+1,high);
+        quickSort(array, low, j - 1);
+        quickSort(array, j + 1, high);
 
-        //klkl
+    }
+
+    /**
+     * 归并排序
+     * @param array
+     */
+    public void mergeSort(int[] array) {
+        int[] temp = new int[array.length];
+        mergeSort(array, 0, array.length - 1, temp);
+
+    }
+
+    private void mergeSort(int[] array, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(array, left, mid, temp);
+            mergeSort(array, mid + 1, right, temp);
+            merge(array, left, mid, right, temp);
+
+        }
+    }
+
+
+    private void merge(int[] array, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int t = 0;
+
+        while (i <= mid && j <= right) {
+            if (array[i] <= array[j]) {
+                temp[t++] = array[i++];
+            } else {
+                temp[t++] = array[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[t++] = array[i++];
+        }
+        while (j <= right) {
+            temp[t++] = array[j++];
+        }
+
+        t = 0;
+        while (left <= right) {
+            array[left++] = temp[t++];
+        }
     }
 
 }
