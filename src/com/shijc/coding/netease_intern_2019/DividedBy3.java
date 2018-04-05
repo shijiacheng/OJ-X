a63561b54899721484c80ad5a4d41948c91a57fc
shijiacheng <iot_shijiacheng@163.com>
2018-04-05 10:17:39 +0800

    2.被3整除

--- src/com/shijc/coding/netease_intern_2019/DividedBy3.java
+++ src/com/shijc/coding/netease_intern_2019/DividedBy3.java
@@ -1,9 +1,9 @@
 package com.shijc.coding.netease_intern_2019;
 
+import java.util.Scanner;
+
 /**
  * 2.被3整除
- * 时间限制：1秒
- * 空间限制：32768K
  * 小Q得到一个神奇的数列: 1, 12, 123,...12345678910,1234567891011...。
  * 并且小Q对于能否被3整除这个性质很感兴趣。
  * 小Q现在希望你能帮他计算一下从数列的第l个到第r个(包含端点)有多少个数可以被3整除。
@@ -20,4 +20,25 @@ package com.shijc.coding.netease_intern_2019;
  * @date 2018/04/05
  */
 public class DividedBy3 {
+    /**
+     * 思路
+     * 序号： 1   2    3     4      5       6        7
+     * 数列： 1  12  123  1234  12345  123456  1234567
+     * 和    1   3    6    10     15      21       28
+     * 模3   1   0    0     1      0       0        1
+     *
+     * 我们可以发现，序号为3n+1的位置（n>=0）的位置不能被3整除，其余都可以
+     */
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int l  = sc.nextInt();
+        int r = sc.nextInt();
+        int result = 0;
+        for (int i = l; i <= r; i++) {
+            if (i%3!=1){
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
 }
